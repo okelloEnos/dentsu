9bee941f4d91c849cc8937f4aff2022e5686f0bc
okelloEnos <enosokello@gmail.com>
2024-03-18 14:27:28 +0300

    complete ui second draft

--- lib/features/dashboard/ui/screens/dashboard_navigation_controller.dart
+++ lib/features/dashboard/ui/screens/dashboard_navigation_controller.dart
@@ -1,7 +1,10 @@
+import 'package:dentsu_test/common_widgets/common_widget_barrel.dart';
 import 'package:dentsu_test/common_widgets/user_photo.dart';
 import 'package:dentsu_test/features/features_barrel.dart';
 import 'package:dentsu_test/util/util_barrel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animated_dialog/flutter_animated_dialog.dart';
+import 'package:go_router/go_router.dart';
 
 class DashboardNavigationController extends StatefulWidget {
   const DashboardNavigationController({Key? key}) : super(key: key);
@@ -32,46 +35,125 @@ class DashboardNavigationControllerState extends State {
     var theme = Theme.of(context);
     String imageUrl = "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
-    return Scaffold(
-      backgroundColor: theme.colorScheme.background,
-      appBar: AppBar(
-        title: Image.asset("assets/images/logo.png",
-          height: 40.0, width: 40.0, color: theme.colorScheme.onPrimary,),
-        actions: _selectedTab == 3 ? [] : [
-          Padding(
-            padding: const EdgeInsets.only(right: 10.0),
-            child: UserPhoto(userProfilePhoto: imageUrl, isOnline: true,),
-          ),
-          IconButton(
-            icon: Icon(Icons.search, color: theme.colorScheme.onPrimary, size: 30.0,),
-            onPressed: () {
-              // todo: Implement search
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.menu, color: theme.colorScheme.onPrimary, size: 30.0,),
-            onPressed: () {
-              // todo: Implement navigation drawer
-            },
-          ),
-        ],
-      ),
-      body: _pages[_selectedTab],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedTab,
-        onTap: (index) => _changeTab(index),
-        items: [
-          BottomNavigationBarItem(icon: Image.asset("assets/images/home.png",
-            height: 20.0, width: 20.0, color: _selectedTab == 0 ? theme.colorScheme.secondary : inactiveGreyColor,), label: "Dashboard"),
-          BottomNavigationBarItem(icon: Image.asset("assets/images/leads.png",
-            height: 20.0, width: 20.0, color: _selectedTab == 1 ? theme.colorScheme.secondary : inactiveGreyColor,), label: "Leads"),
-          BottomNavigationBarItem(
-              icon: Image.asset("assets/images/quotes.png",
-                height: 20.0, width: 20.0, color: _selectedTab == 2 ? theme.colorScheme.secondary : inactiveGreyColor,), label: "Quotes"),
-          BottomNavigationBarItem(
-              icon: Image.asset("assets/images/profile.png",
-                height: 20.0, width: 20.0, color: _selectedTab == 3 ? theme.colorScheme.secondary : inactiveGreyColor,), label: "Profile"),
-        ],
+    return WillPopScope(
+      onWillPop: () async {
+        var actionSelected = await showAnimatedDialog<bool>(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              content: Text(
+                'Do you wish to exit the app?',
+                style: TextStyle(
+                    fontFamily: 'Dm Sans',
+                    fontSize: 18.0,
+                    color: theme.colorScheme.primary),
+              ),
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: GestureDetector(
+                            onTap: () => context.pop(true),
+                            child: Container(
+                              padding: const EdgeInsets.only(
+                                  left: 20.0, right: 20.0, top: 5.0, bottom: 5.0),
+                              decoration: BoxDecoration(
+                                  color: theme.colorScheme.error,
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(5.0))),
+                              child: Center(
+                                child: CustomTextWidget(
+                                  text: "Yes",
+                                  color: theme.colorScheme.onError,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w700,
+
+                                ),
+                              ),
+                            )),
+                      ),
+                      const SizedBox(
+                        width: 10.0,
+                      ),
+                      Expanded(
+                        child: GestureDetector(
+                            onTap: () => context.pop(false),
+                            child: Container(
+                              padding: const EdgeInsets.only(
+                                  left: 20.0, right: 20.0, top: 5.0, bottom: 5.0),
+                              decoration: BoxDecoration(
+                                  color: theme.colorScheme.secondary,
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(5.0))),
+                              child: Center(
+                                child: CustomTextWidget(
+                                  text: "No",
+                                  color: theme.colorScheme.onSecondary,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+          animationType: DialogTransitionType.slideFromTop,
+          curve: Curves.fastOutSlowIn,
+          duration: const Duration(seconds: 1),
+        );
+
+        return actionSelected ?? false;
+      },
+      child: Scaffold(
+        backgroundColor: theme.colorScheme.background,
+        appBar: AppBar(
+          title: Image.asset("assets/images/logo.png",
+            height: 40.0, width: 40.0, color: theme.colorScheme.onPrimary,),
+          actions: _selectedTab == 3 ? [] : [
+            Padding(
+              padding: const EdgeInsets.only(right: 10.0),
+              child: UserPhoto(userProfilePhoto: imageUrl, isOnline: true,),
+            ),
+            IconButton(
+              icon: Icon(Icons.search, color: theme.colorScheme.onPrimary, size: 30.0,),
+              onPressed: () {
+                // todo: Implement search
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.menu, color: theme.colorScheme.onPrimary, size: 30.0,),
+              onPressed: () {
+                // todo: Implement navigation drawer
+              },
+            ),
+          ],
+        ),
+        body: _pages[_selectedTab],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedTab,
+          onTap: (index) => _changeTab(index),
+          items: [
+            BottomNavigationBarItem(icon: Image.asset("assets/images/home.png",
+              height: 20.0, width: 20.0, color: _selectedTab == 0 ? theme.colorScheme.secondary : inactiveGreyColor,), label: "Dashboard"),
+            BottomNavigationBarItem(icon: Image.asset("assets/images/leads.png",
+              height: 20.0, width: 20.0, color: _selectedTab == 1 ? theme.colorScheme.secondary : inactiveGreyColor,), label: "Leads"),
+            BottomNavigationBarItem(
+                icon: Image.asset("assets/images/quotes.png",
+                  height: 20.0, width: 20.0, color: _selectedTab == 2 ? theme.colorScheme.secondary : inactiveGreyColor,), label: "Quotes"),
+            BottomNavigationBarItem(
+                icon: Image.asset("assets/images/profile.png",
+                  height: 20.0, width: 20.0, color: _selectedTab == 3 ? theme.colorScheme.secondary : inactiveGreyColor,), label: "Profile"),
+          ],
+        ),
       ),
     );
   }
